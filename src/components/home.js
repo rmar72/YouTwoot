160b4b29339868fa42efdb9c0bf1947e1174618a
Jason <jasonsoloriom@gmail.com>
2018-09-05 20:36:44 -0600

    Removed nav, Imported new NavBar and used it to display on the page

--- src/components/home.js
+++ src/components/home.js
@@ -1,25 +1,10 @@
 import React from 'react';
-
+import NavBar from './navbar';
 
 const Home = () =>{
   return(
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-        <a className="navbar-brand font-italic" href="#">YouTwoot</a>
-        <div>
-           <ul className="navbar-nav">
-             <li className="nav-item">
-               <a className="nav-link" href="/">Home </a>
-             </li>
-             <li className="nav-item">
-               <a className="nav-link" href="/signup">Sign Up</a>
-             </li>
-             <li className="nav-item">
-               <a className="nav-link" href="/youtweet">YT</a>
-             </li>
-           </ul>
-        </div>
-      </nav>
+      <NavBar/>
 
       <div className="jumbotron">
         <h1 className="display-4">Welcome to YouTwoot</h1>
