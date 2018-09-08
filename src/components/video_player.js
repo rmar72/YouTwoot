33d283b8fbfecf71cd9e160f363cd9bae7e64f4b
Jason <jasonsoloriom@gmail.com>
2018-09-08 17:53:49 -0600

    Adding a new li, which holds the channel name. Removing the li that had the description, and replaced it with a bootstrap collapse. So now the description is collapsable

--- src/components/video_player.js
+++ src/components/video_player.js
@@ -14,13 +14,16 @@ const VideoPlayer = ({video}) => {
             <iframe className="embed-responsive-item" src={url}></iframe>
           </div>
           <ul className="list-group">
-            <li className="list-group-item">{video.snippet.title}</li>
-            <li className="list-group-item">{video.snippet.description}</li>
+            <li className="list-group-item list-group-item-danger text-center font-weight-bold">{video.snippet.channelTitle}</li>
+            <li className="list-group-item list-group-item-secondary text-center text-monospace">{video.snippet.title}</li>
+            <button className="btn btn-info" data-toggle="collapse" data-target="#content">Description</button>
+            <div className="collapse" id="content">
+              {video.snippet.description}
+            </div>
           </ul>
         </div>
       </div>
     </div>
-
   );
 };
 
