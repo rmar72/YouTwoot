56db08b983139fce3f69cd8ccf92415f1df15799
Jason <jasonsoloriom@gmail.com>
2018-09-10 22:51:02 -0600

    allowFullScreen added, 2 additional divs that display trending video status, and the other the number of views the video has.
    Problem is that the youtube-api-search api is conflicting with the part 
    'statistics'

--- src/components/video_player.js
+++ src/components/video_player.js
@@ -5,23 +5,27 @@ const VideoPlayer = ({video}) => {
     return <div>Loading...</div>
   }
   const videoId = video.id;
+  const videoCount = numeral(video.statistics.viewCount).format('0,0');
   const url = `https://www.youtube.com/embed/${videoId}`;
   return (
     <div className="container">
       <div className="row">
+        <div className="col-2 trending"> <span className="highlight">#1</span> Trending Video</div>
         <div className="col-8">
           <div className="embed-responsive embed-responsive-16by9">
-            <iframe className="embed-responsive-item" src={url}></iframe>
+            <iframe className="embed-responsive-item" src={url} allowFullScreen></iframe>
           </div>
           <ul className="list-group">
             <li className="list-group-item list-group-item-danger text-center font-weight-bold">{video.snippet.channelTitle}</li>
             <li className="list-group-item list-group-item-secondary text-center text-monospace">{video.snippet.title}</li>
             <button className="btn btn-info" data-toggle="collapse" data-target="#content">Description</button>
-            <div className="collapse" id="content">
+            <div className="collapse description" id="content">
               {video.snippet.description}
             </div>
           </ul>
         </div>
+        <div className="col-2 trending">Total Views: <span className="highlight">{videoCount}</span></div>
+
       </div>
     </div>
   );
