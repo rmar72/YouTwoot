3218251d22fbc9d84cf8ae9af05ca242fd8adbb9
Jason <jasonsoloriom@gmail.com>
2018-10-15 23:04:52 -0600

    Removed the reactstrap form and turned it into a form using bootstrap like the rest of the app

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Col, Button, Form, FormGroup, Label, Input, FormText, Container, Row} from 'reactstrap';
+import Axios from 'axios';
 
 class SignUp extends Component{
   constructor(props){
@@ -18,37 +18,39 @@ handlePasswordChange = (e) =>{
   this.setState({password: e.target.value})
 };
 handleLogin = () =>{
-  console.log('Email: ' + this.state.email);
-  console.log('Password: ' + this.state.password);
+  // console.log('Email: ' + this.state.email);
+  // console.log('Password: ' + this.state.password);
+  Axios.post('/api/users', {user: this.state})
 };
 
   render(){
     return(
-      <div>
-          <Container>
-            <form>
-              <h1 >Sign up!</h1>
-            <div className="form-group">
-              <label>Enter Email</label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Email"
-                value={this.state.email}
-                onChange={this.handleEmailChange}></input>
-            </div>
-            <div className="form-group">
-              <label>Enter Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-                value={this.state.password}
-                onChange={this.handlePasswordChange}></input>
-            </div>
-            <button type="submit" className="btn btn-info" onClick={this.handleLogin}>Join the FUN!</button>
-          </form>
-        </Container>
+      <div className="container">
+        <form>
+          <h1>Sign up</h1>
+
+          <div className="form-group">
+            <label className="control-label">Username</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.handleEmailChange}></input>
+          </div>
+
+          <div className="form-group">
+            <label className="control-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handlePasswordChange}></input>
+          </div>
+
+          <button type="submit" className="btn btn-info" onClick={this.handleLogin}>Join the Fun!</button>
+        </form>
       </div>
     )
   }
