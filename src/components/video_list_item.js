55ef52940c646d5e149d3a7829ccf214775aa1c3
Jason <jasonsoloriom@gmail.com>
2018-10-16 15:15:02 -0600

    Moved class below the li

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -4,12 +4,13 @@ const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return(
-    <li onClick={() => onVideoSelect(video)} className="list-group-item">
-      <div className="media">
-          <img className="mr-3" src={imageUrl}/>
-         <div className="title">{video.snippet.title}</div>
-     </div>
-   </li>
+      <li onClick={() => onVideoSelect(video)}
+        className="list-group-item">
+        <div className="media">
+            <img className="mr-3" src={imageUrl}/>
+           <div className="title">{video.snippet.title}</div>
+       </div>
+     </li>
   )
 };
 
