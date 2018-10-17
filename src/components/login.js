86c60d75f4d26e09f64e8b3aac3d1e0dd12999b7
Jason Solorio <jasonsoloriom@gmail.com>
2018-10-17 13:43:36 -0600

    A login form to allow a created user to log back into youtwoot. Form only needs for a user to provide email and password,

--- src/components/login.js
+++ src/components/login.js
@@ -1,23 +1,70 @@
 import React, {Component} from 'react';
 
 class Login extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleEmailSend = (e) =>{
+    this.setState({email: e.target.value})
+  }
+
+  handlePasswordSend = (e) =>{
+    this.setState({password: e.target.value})
+  }
+
+  handleTheLogin = () =>{
+    fetch('http://localhost:3000/auth/login',{
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        user: {
+          email: this.state.email,
+          password: this.state.password
+        }
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      localStorage.setItem('token', data.user.token);
+    });
+  }
+
   render(){
     return(
       <div className="container">
         <form>
           <h1>Enter Information</h1>
-
           <div className="form-group">
-            <label className="control-label">Username</label>
-            <input className="form-control"></input>
+            <label className="control-label">Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Your email"
+              className="form-control"
+              value={this.state.email}
+              onChange={this.handleEmailSend}
+              ></input>
           </div>
 
           <div className="form-group">
             <label className="control-label">Password</label>
-            <input className="form-control"></input>
+            <input
+              name="password"
+              placeholder="Your password"
+              type="password"
+              className="form-control"
+              value={this.state.password}
+              onChange={this.handlePasswordSend}
+              ></input>
           </div>
 
-          <button type="submit" className="btn btn-info" >Log In</button>
+          <button className="btn btn-info" type="button" onClick={this.handleTheLogin}>Log In</button>
         </form>
       </div>
     )
