121a24d563205ba1f49fcc0c5ac4ce348a0b374f
rsol7 <rsolm72@gmail.com>
2018-10-18 01:05:47 -0600

    fn sending video id and stopping propagation for button click

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,6 +3,32 @@ import React from 'react';
 const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
 
+
+  function tweetVideo(event, vid){
+    event.stopPropagation();
+
+    const token = localStorage.getItem('token');
+
+    if(token){
+      fetch('http://localhost:3000/users/tweet-video',{
+        method:'POST',
+        headers: { 
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token}`
+        },
+        body: JSON.stringify({
+          video: {
+            url: vid.id
+          }
+        })
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+    }
+ 
+  }
+
   return(
       <li onClick={() => onVideoSelect(video)}
         className="list-group-item">
