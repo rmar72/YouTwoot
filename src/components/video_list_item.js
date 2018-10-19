770242d809faf47800b8a47534f694edce930126
Jason Solorio <jasonsoloriom@gmail.com>
2018-10-18 23:51:41 -0600

    Instead of breaking the li into two lines, put it back into one line

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -4,8 +4,7 @@ const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return(
-      <li onClick={() => onVideoSelect(video)}
-        className="list-group-item">
+      <li onClick={() => onVideoSelect(video)} className="list-group-item">
         <div className="media">
             <img className="mr-3" src={imageUrl}/>
            <div className="title">{video.snippet.title}</div>
