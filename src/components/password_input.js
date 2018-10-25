dfa495eb3f2a051c9d7a5707c70402923817798d
rsol7 <rsolm72@gmail.com>
2018-10-24 20:26:45 -0600

    New PasswordInput Class Component

---
+++ src/components/password_input.js
@@ -0,0 +1,18 @@
+import react, {Component} from 'react';
+
+class PasswordInput extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            password: '',
+            validPassword: null
+        }
+    }
+
+
+    
+
+}
+
+export default PasswordInput;
