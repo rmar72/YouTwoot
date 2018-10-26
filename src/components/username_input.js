e4f783f1e33e424bdf96daf3f02e305be53c69e6
rsol7 <rsolm72@gmail.com>
2018-10-25 22:14:21 -0600

    new username input component

---
+++ src/components/username_input.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+class UsernameInput extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            username: "",
+            validUsername: null
+        }
+    }
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default UsernameInput;
