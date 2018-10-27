0158940ba07017d310a3704567750e4b47f3bc86
rsol7 <rsolm72@gmail.com>
2018-10-27 01:42:10 -0600

    code clean up

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,47 +1,40 @@
-import React, {Component} from 'react';
-import UsernameInput from './username_input';
-import EmailInput from './email_input';
-import PasswordInput from './password_input';
+import React, {Component} from "react";
+import UsernameInput from "./username_input";
+import EmailInput from "./email_input";
+import PasswordInput from "./password_input";
 
-
-class SignUp extends Component{
+class SignUp extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      username: '',
-      email: '',
-      password: ''
+      username: "",
+      email: "",
+      password: ""
     }
   }
-usernameInputHandler = (e) =>{
-  this.setState({username: e.target.value});
-};
 
-handleSignUp = () =>{
-  alert(1)
-  if( this.state.username && this.state.email && this.state.password){
-      fetch('http://localhost:3000/auth/register', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          user: {
-            username: this.state.username,
-            email: this.state.email,
-            password: this.state.password
-          }
+  handleSignUp = () => {
+    if( this.state.username && this.state.email && this.state.password){
+      
+        fetch("http://localhost:3000/auth/register", {
+          method: "POST",
+          headers: { "Content-Type": "application/json"},
+          body: JSON.stringify({
+            user: {
+              username: this.state.username,
+              email: this.state.email,
+              password: this.state.password
+            }
+          })
         })
-      })
-      .then(response => response.json())
-      .then(data => {
-        localStorage.setItem('token', data.user.token);
-      });
-  }
+        .then(response => response.json())
+        .then(data => {
+          localStorage.setItem("token", data.user.token);
+        });
+    }
 
-};
-  isValid = () => {
-    return true;
-  }
+  };
 
   render(){
     return(
@@ -60,9 +53,8 @@ handleSignUp = () =>{
               passwordValue={password => this.setState({password})} />
 
             <button
-              className="btn btn-info disabled" 
+              className="btn btn-info" 
               type="button" 
-              disabled={this.isValid()} 
               onClick={this.handleSignUp}
             > 
               Join the fun!
