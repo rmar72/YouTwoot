6ceaeca1bd2127c95812c0ccce75d0c35da909f5
rsol7 <rsolm72@gmail.com>
2018-10-27 16:29:07 -0600

    encoding URI to build proper urls for web intents api

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -28,7 +28,9 @@ const VideoListItem = ({video, onVideoSelect}) => {
     }
  
   }
-
+  
+  const text = video.snippet.title;
+  console.log(encodeURIComponent(text))
   return(
       <li onClick={() => onVideoSelect(video)} className="list-group-item">
         <div className="media">
@@ -40,9 +42,13 @@ const VideoListItem = ({video, onVideoSelect}) => {
           <a type="button" 
             className="btn btn-block btn-social btn-twitter"
             onClick={(e) => tweetVideo(e, video)}
+            href={`https://twitter.com/intent/tweet?url=https%3A//youtu.be/${video.id}&text=${text}&via=YouTube&related=YouTube,YouTubeTrends,YTCreators`}
+            target="_blank"
+            // href={`https://twitter.com/intent/tweet?url=https%3A//youtu.be/h2DboKOvWIc&text=Dog%20Was%20SO%20Proud%20When%20Her%20Human%20Sister%20Finally%20Walked%20%20|%20The%20Dodo&via=YouTube&related=YouTube,YouTubeTrends,YTCreators`}
           >
             <span className="fa fa-twitter"></span> Tweet Me!
           </a>
+
         </div>
 
       </li>
